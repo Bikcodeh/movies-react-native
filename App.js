7cbd2beff71e0ba00c4b51eed491c5cf2a9e671f
Bikcode <bikcodeh@gmail.com>
2022-12-30 17:45:39 -0500

    partial creation of context to handle preferences

--- App.js
+++ App.js
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, {useMemo, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Provider as PaperProvider} from 'react-native-paper';
 import Navigation from './src/navigation/Navigation';
 import {StatusBar} from 'react-native';
 
 import MoviesTheme from './src/theme/Theme';
+import PreferencesContext from './src/context/PreferencesContext';
 
 const App = () => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+
+  const preference = useMemo(
+    () => ({
+      toggleTheme,
+      theme,
+    }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [theme],
+  );
   return (
-    <PaperProvider theme={MoviesTheme.DarkThemePaper}>
-      <NavigationContainer theme={MoviesTheme.DarkThemeNavigation}>
-        <StatusBar barStyle={'dark-content'} />
-        <Navigation />
-      </NavigationContainer>
-    </PaperProvider>
+    <PreferencesContext.Provider value={preference}>
+      <PaperProvider
+        theme={
+          theme === 'dark'
+            ? MoviesTheme.DarkThemePaper
+            : MoviesTheme.DefaultThemePaper
+        }>
+        <NavigationContainer
+          theme={
+            theme === 'dark'
+              ? MoviesTheme.DarkThemeNavigation
+              : MoviesTheme.DefaultThemeNavigation
+          }>
+          <StatusBar
+            barStyle={theme === 'dark' ? 'dark-content' : 'light-content'}
+          />
+          <Navigation />
+        </NavigationContainer>
+      </PaperProvider>
+    </PreferencesContext.Provider>
   );
 };
 export default App;
