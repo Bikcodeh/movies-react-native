d08717cf2a6fcf575352f53bdbf7d4f334098768
Bikcode <bikcodeh@gmail.com>
2023-01-02 07:30:25 -0500

    display all images in the carousel

--- src/components/CarouselVertical.tsx
+++ src/components/CarouselVertical.tsx
@@ -1,33 +1,67 @@
-import { Dimensions, Text, View } from 'react-native';
+import React from 'react';
+import {
+  Dimensions,
+  Image,
+  TouchableWithoutFeedback,
+  View,
+  StyleSheet,
+} from 'react-native';
 import Carousel from 'react-native-snap-carousel';
-import { Movie } from '../interfaces/movieinterfaces';
+import {Movie} from '../interfaces/movieinterfaces';
+import {Title} from 'react-native-paper';
+import {BASE_URL_IMG} from '../utils/constants';
 
 const WINDOW_WIDTH = Dimensions.get('window').width;
 const ITEM_WIDTH = Math.round(WINDOW_WIDTH * 0.7);
 
 interface Props {
-    movies: Movie[]
+  movies: Movie[];
 }
 
 export default function CarouselVertical(props: Props) {
-    return (
-        <Carousel
-            layout='default'
-            data={props.movies}
-            sliderWidth={WINDOW_WIDTH}
-            itemWidth={ITEM_WIDTH}
-            renderItem={(item) => <RenderItem title='Movie' />}
-        />
-    )
+  return (
+    <Carousel
+      layout="default"
+      data={props.movies}
+      sliderWidth={WINDOW_WIDTH}
+      itemWidth={ITEM_WIDTH}
+      renderItem={item => <RenderItem movie={item.item} />}
+    />
+  );
 }
 interface RenderItemProps {
-    title: string
+  movie: Movie;
 }
 
-const RenderItem = ({title}: RenderItemProps) => {
-    return (
-        <View>
-            <Text>{title}</Text>
-        </View>
-    )
-}
+const RenderItem = ({movie}: RenderItemProps) => {
+  const imageUrl = `${BASE_URL_IMG}/${movie.poster_path}`;
+  return (
+    <TouchableWithoutFeedback onPress={() => console.log(movie)}>
+      <View style={styles.card}>
+        <Image style={styles.image} source={{uri: imageUrl}} />
+        <Title style={styles.title}>{movie.title}</Title>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 10,
+  },
+  image: {
+    width: '100%',
+    height: 450,
+    borderRadius: 20,
+  },
+  title: {
+    marginHorizontal: 10,
+    marginTop: 10,
+  },
+});
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,34 +1,31 @@
-import React, { useEffect } from 'react';
-import { Text, View, ScrollView, StyleSheet } from 'react-native';
+import React from 'react';
+import {View, ScrollView, StyleSheet} from 'react-native';
 import useNewMovies from '../hooks/useNewMovies';
-import { Title } from 'react-native-paper';
+import {Title} from 'react-native-paper';
 import CarouselVertical from '../components/CarouselVertical';
 
 export default function Home() {
-  const { isLoading, newMovies, hasError } = useNewMovies();  
+  const {isLoading, newMovies, hasError} = useNewMovies();
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
-      {
-        newMovies && (
-          <View style={styles.news}>
-              <Title style={styles.newsTitle}>New Movies</Title>
-              <CarouselVertical movies={newMovies}/>
-          </View>
-        )
-      }
-      {newMovies.map(movie => <Text key={movie.id}>{movie.title}</Text>)}
+      {newMovies && (
+        <View style={styles.news}>
+          <Title style={styles.newsTitle}>New Movies</Title>
+          <CarouselVertical movies={newMovies} />
+        </View>
+      )}
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   news: {
-    marginVertical: 16
+    marginVertical: 16,
   },
   newsTitle: {
     marginBottom: 16,
     marginHorizontal: 16,
     fontWeight: 'bold',
-    fontSize: 22
-  }
-})
+    fontSize: 22,
+  },
+});
